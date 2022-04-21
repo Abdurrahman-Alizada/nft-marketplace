49ca3895d0e5fb32aabcc5db77e8622a4e2708a9
Abdurrahman-Alizada <abdurrahmanalizada17@gmail.com>
2022-04-21 21:15:01 +0500

    minimal snippest uploaded

---
+++ src/pages/menu.js
@@ -0,0 +1,80 @@
+const menus = [
+    {
+        id: 2,
+        name: 'Explore',
+        namesub: [
+            {
+                id: 1,
+                sub: 'Explore',
+                links: '/explore'
+            },
+
+            {
+                id: 3,
+                sub: 'Creator',
+                links: '/creator'
+            },
+            {
+                id: 4,
+                sub: 'Item',
+                links: '/item'
+            },
+            {
+                id: 5,
+                sub: 'Item Details',
+                links: '/item-details'
+            },
+            
+        ],
+    },
+    {
+        id: 3,
+        name: 'Community',
+        namesub: [
+            {
+                id: 1,
+                sub: 'Blog',
+                links: '/blog'
+            },
+            {
+                id: 2,
+                sub: 'Blog Details',
+                links: '/blog-details'
+            },
+        ],
+    },
+    {
+        id: 4,
+        name: 'Page',
+        namesub: [
+            {
+                id: 1,
+                sub: 'Authors',
+                links: '/authors'
+            },
+            {
+                id: 2,
+                sub: 'Wallet Connect',
+                links: '/connect-wallet'
+            },
+            {
+                id: 3,
+                sub: 'Create Item',
+                links: '/create-item'
+            },
+            {
+                id: 4,
+                sub: 'Login',
+                links: '/login'
+            },
+            {
+                id: 5,
+                sub: 'Register',
+                links: '/register'
+            },
+        ],
+    },
+    
+]
+
+export default menus;
